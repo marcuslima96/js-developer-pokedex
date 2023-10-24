88339e6a2fb860ba92759549fcdcbc5684cd3f12
marcuslima96 <marcusvsl33@gmail.com>
2023-10-24 12:31:48 -0300

    adição de stats dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,49 +10,43 @@ let pokeList = [];
 
 function convertPokemonToLi(pokemon) {
   return `
-
-        <li class="pokemon ${pokemon.type}">
-            
-        <span class="number">${pokemon.number}</span>
-        <button class="btn-pokemon" onClick="pokemonModal(${pokemon.number})">
-            <span class="name" >${pokemon.name}</span>
-            </button>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types
-                      .map((type) => `<li class="type ${type}">${type}</li>`)
-                      .join("")}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `;
+    <li class="pokemon ${pokemon.type}">
+      <span class="number">${pokemon.number}</span>
+      <button class="btn-pokemon" onClick="pokemonModal(${pokemon.number})">
+        <span class="name">${pokemon.name}</span>
+      </button>
+      <div class="detail">
+        <ol class="types">
+          ${pokemon.types
+            .map((type) => `<li class="type ${type}">${type}</li>`)
+            .join("")}
+        </ol>
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+      </div>
+    </li>
+  `;
 }
 
-function loadPokemonItens(offset, limit) {
+function loadPokemonItems(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    // pokeList = [...pokeList, ...pokemons];
-    // pokeList.push(pokemons);
     const newHtml = pokemons.map(convertPokemonToLi).join("");
     pokemonList.innerHTML += newHtml;
   });
 }
 
-loadPokemonItens(offset, limit);
+loadPokemonItems(offset, limit);
 
 loadMoreButton.addEventListener("click", () => {
   offset += limit;
-  const qtdRecordsWithNexPage = offset + limit;
+  const qtdRecordsWithNextPage = offset + limit;
 
-  if (qtdRecordsWithNexPage >= maxRecords) {
+  if (qtdRecordsWithNextPage >= maxRecords) {
     const newLimit = maxRecords - offset;
-    loadPokemonItens(offset, newLimit);
+    loadPokemonItems(offset, newLimit);
 
     loadMoreButton.parentElement.removeChild(loadMoreButton);
   } else {
-    loadPokemonItens(offset, limit);
+    loadPokemonItems(offset, limit);
   }
 });
 
@@ -64,69 +58,49 @@ async function pokemonModal(id) {
       throw new Error("Não foi possível buscar os dados do Pokémon.");
     }
     let pokemonData = await response.json();
-    const dialog = document.getElementById("dialog");
-    console.log(pokemonData);
 
-    dialog.innerHTML = "";
-
-    dialog.innerHTML = `<button type="button" class="btnBack" onclick="closeModal()">
-             &#129044;
-            </button>
-            <div>
-            <span class="number">#${pokemonData.id}</span>
-            <span class="name">${pokemonData.name}</span>
-            </div>
-           `;
-
-    const types = pokemonData.types.map((type) => type.type.name);
-    let typesList = "";
-
-    if (types.length > 0) {
-      typesList += `<ol class="types">`;
-      types.forEach((typeName) => {
-        typesList += `<li class="type ${typeName}">${typeName}</li>`;
-      });
-      typesList += `</ol>`;
-    } else {
-      typesList = `<p class="type">Tipo(s): Desconhecido</p>`;
-    }
-
-    dialog.classList.add(`${types[0]}`);
-
-    dialog.innerHTML += typesList;
-
-    const abilities = pokemonData.abilities
-      .map((ability) => ability.ability.name)
-      .join(" | ");
-
-    dialog.innerHTML += `
-    <div class="img">
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-      pokemonData.id
-    }.png" alt="${pokemonData.name}"> </div>
+    dialog.innerHTML = `
+      <button type="button" class="btnBack" onclick="closeModal()">&#129044;</button>
+      <div>
+        <span class="number">#${pokemonData.id}</span>
+        <span class="name">${pokemonData.name}</span>
+      </div>
+      <div class="img">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+        pokemonData.id
+      }.png" alt="${pokemonData.name}">
+    </div>
+    
       <div class="info">
         <h3>Weight</h3><p class="weight">${pokemonData.weight / 10} kg</p>
         <h3>Height</h3><p class="height">${pokemonData.height / 10} m</p>
-        <h3>Abilities</h3><p class="abilities">${abilities}</p>
+        <h3>Abilities</h3><p class="abilities">${pokemonData.abilities
+          .map((ability) => ability.ability.name)
+          .join(" | ")}</p>
+        <h3>Stats</h3>
+        <ul class="stats">
+          ${pokemonData.stats
+            .map(
+              (stat) =>
+                `<li class="${pokemonData.types[0].type.name}">${stat.stat.name}: ${stat.base_stat}</li>`
+            )
+            .join("")}
+        </ul>
       </div>
     `;
 
-    // if (pokemonData.stats.length > 0) {
-    //   pokemonData.stats.forEach((stat) => {
-    //     const statName = stat.stat.name;
-    //     const statBase = stat.base_stat;
-    //     dialog.innerHTML += `<ul> <li>${statName}: ${statBase}</li> </ul> `;
-    //   });
-    // } else {
-    //   dialog.innerHTML += "<p class='stats'>Stats Base: Desconhecido</p>";
-    // }
+    dialog.classList.add(pokemonData.types[0].type.name);
+
+    // Adicione o evento 'load' para mostrar o dialog após o conteúdo ter sido carregado
+    dialog.addEventListener("load", function () {
+      dialog.showModal();
+    });
   } catch (error) {
     console.error(error);
   }
 }
 
 function closeModal() {
-  const dialog = document.getElementById("dialog");
   dialog.close();
   dialog.classList.remove(...dialog.classList);
 }
